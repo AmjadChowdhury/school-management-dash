bc69d6c5a717e0027dfc6f165c1e72f7e79ae97c
AmjadChowdhury <amjadchowdhury247@gmail.com>
2026-01-08 00:05:47 +0600

    create 4 cards in admin section and add two chart simple radial bar chart and simple bar chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 const AdminPage = () => {
-    return (
-        <div>
-            admin
+  return (
+    <div className="p-4 flex flex-col md:flex-row gap-4">
+      {/* left */}
+      <div className="w-full md:w-2/3 flex flex-col gap-8">
+        {/* USER card */}
+        <div className="flex gap-4 justify-between flex-wrap lg:flex-nowrap">
+            <UserCard type="student"/>
+            <UserCard type="teacher"/>
+            <UserCard type="parent"/>
+            <UserCard type="staff"/>
         </div>
-    );
+        {/* Middle Charts */}
+        <div className="flex gap-4 flex-col md:flex-row">
+            {/* CountCahrts */}
+            <div className="w-full md:w-1/3 h-[450px] ">
+                <CountChart/>
+            </div>
+            {/* Attendance chart */}
+            <div className="w-full md:w-2/3 h-[450px]">
+                <AttendanceChart/>
+            </div>
+        </div>
+        {/* Bottom Charts */}
+        <div className=""></div>
+      </div>
+      {/* right */}
+      <div className="w-full md:1/3">r</div>
+    </div>
+  );
 };
 
 export default AdminPage;
---
+++ src/components/CountChart.tsx
@@ -0,0 +1,82 @@
+"use client"
+import React from "react";
+import {
+  RadialBarChart,
+  RadialBar,
+  Legend,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+import Image from "next/image";
+
+// #region Sample data
+const data = [
+  {
+    name: "Total",
+    count: 100,
+    fill: "white",
+  },
+  {
+    name: "Girls",
+    count: 52,
+    fill: "#888be6",
+  },
+  {
+    name: "Boys",
+    count: 48,
+    fill: "#d3d886",
+  }
+  
+ 
+];
+
+
+
+const CountChart = () => {
+  return (
+    <div className="bg-white rounded-xl w-full h-full p-4">
+      {/* title */}
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Students</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
+      </div>
+      {/* Chart */}
+      <div className="w-full h-[75%]">
+        <ResponsiveContainer>
+          <RadialBarChart
+            cx="50%"
+            cy="50%"
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={32}
+            data={data}
+          >
+            <RadialBar
+              label={{ position: "insideStart", fill: "#fff" }}
+              background
+              dataKey="count"
+            />
+            <Legend iconSize={10} layout="vertical" verticalAlign="middle"/>
+            <Tooltip/>
+          </RadialBarChart>
+        </ResponsiveContainer>
+      </div>
+      {/* bottom */}
+      <div className="flex justify-center gap-16">
+        <div className="flex flex-col gap-1">
+          <div className="w-5 h-5 bg-[#d3d886] rounded-full" />
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-xs text-gray-300">Boys (55%)</h2>
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="w-5 h-5 bg-[#888be6] rounded-full" />
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-xs text-gray-300">Girls (55%)</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountChart;
