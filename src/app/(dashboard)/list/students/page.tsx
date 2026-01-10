d516a9f19f2efc114a924d1c0a8a91d3ccdfe63c
AmjadChowdhury <amjadchowdhury247@gmail.com>
2026-01-10 19:21:11 +0600

    added forms for student and teacher to create new lists

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -1,4 +1,4 @@
-// import FormModal from "@/components/FormModal";
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -74,17 +74,17 @@ const StudentListPage = () => {
       <td className="hidden md:table-cell">{item.address}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+          <Link href={`/list/students/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-[#E9D5FF]">
               <Image src="/view.png" alt="" width={16} height={16} />
             </button>
           </Link>
-          {/* {role === "admin" && (
+          {role === "admin" && (
             // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
             //   <Image src="/delete.png" alt="" width={16} height={16} />
             // </button>
             <FormModal table="student" type="delete" id={item.id}/>
-          )} */}
+          )}
         </div>
       </td>
     </tr>
@@ -98,18 +98,18 @@ const StudentListPage = () => {
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-[#E0F2FE]">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-[#E0F2FE]">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {/* {role === "admin" && (
-              // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            {role === "admin" && (
+              // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-[#E0F2FE]">
               //   <Image src="/plus.png" alt="" width={14} height={14} />
               // </button>
               <FormModal table="student" type="create"/>
-            )} */}
+            )}
           </div>
         </div>
       </div>
